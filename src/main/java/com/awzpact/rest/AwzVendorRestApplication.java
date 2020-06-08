67a2d9c47b15f1a256b2344920c483763939afdb
SinghAmit27 <akss187@gmail.com>
2020-06-08 21:43:07 +0530

    first commit

--- src/main/java/com/awzpact/rest/AwzVendorRestApplication.java
+++ src/main/java/com/awzpact/rest/AwzVendorRestApplication.java
@@ -2,8 +2,10 @@ package com.awzpact.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.awzpact.rest"})
 public class AwzVendorRestApplication {
 
 	public static void main(String[] args) {
